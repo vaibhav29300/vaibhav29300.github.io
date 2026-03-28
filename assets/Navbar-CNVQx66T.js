b2012906ffb8cb2412798ab51e9090b05aadc4e4
vaishe_syncron <vaibhav.shekhar@syncron.com>
2026-03-28 17:38:10 +0530

    Updates

--- assets/Navbar-CNVQx66T.js
+++ assets/Navbar-CNVQx66T.js
@@ -1,4 +1,4 @@
-import{j,r as bt}from"./index-mcN3gknz.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
+import{j,r as bt}from"./index-SzsVuroX.js";import{g as St,S as yt}from"./ScrollTrigger-0lJgtTtq.js";/*!
  * SplitText 3.14.2
  * https://gsap.com
  *
